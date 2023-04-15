fee93d1807582a7d87951a6dbd962d32773147af
mecaneer23 <mecaneer23@gmail.com>
2023-04-15 01:05:35 -0500

    add view colors

---
+++ view-colors.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("#\033[30m#\033[31m#\033[32m#\033[33m#\033[34m#\033[35m#\033[36m#\033[37m\n#\033[1;30m#\033[1;31m#\033[1;32m#\033[1;33m#\033[1;34m#\033[1;35m#\033[1;36m#\033[1;37m#\033[0m")
+
+# Todo: display larger blocks instead of just one character per color
+# instead of directly inserting escape codes, use a for loop with a column index and querying lists
