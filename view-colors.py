050e476398804ddb1a4ad463484c2be4f7fa8450
mecaneer23 <mecaneer23@gmail.com>
2023-04-15 01:28:43 -0500

    implement todos

--- view-colors.py
+++ view-colors.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
-print("#\033[30m#\033[31m#\033[32m#\033[33m#\033[34m#\033[35m#\033[36m#\033[37m\n#\033[1;30m#\033[1;31m#\033[1;32m#\033[1;33m#\033[1;34m#\033[1;35m#\033[1;36m#\033[1;37m#\033[0m")
+regular_colors = [
+    "\033[0;30m",
+    "\033[0;31m",
+    "\033[0;32m",
+    "\033[0;33m",
+    "\033[0;34m",
+    "\033[0;35m",
+    "\033[0;36m",
+    "\033[0;37m",
+]
 
-# Todo: display larger blocks instead of just one character per color
-# instead of directly inserting escape codes, use a for loop with a column index and querying lists
+bold_colors = [
+    "\033[1;30m",
+    "\033[1;31m",
+    "\033[1;32m",
+    "\033[1;33m",
+    "\033[1;34m",
+    "\033[1;35m",
+    "\033[1;36m",
+    "\033[1;37m",
+]
+
+color_names = [
+    "Black",
+    "Red",
+    "Green",
+    "Yellow",
+    "Blue",
+    "Magenta",
+    "Cyan",
+    "White",
+]
+
+
+for i, (v1, v2) in enumerate(zip(regular_colors, bold_colors)):
+    print("\033[0m" + color_names[i].ljust(10) + " " * 5 + v1 + "█" * 5 + " " + v2 + "█" * 5)
